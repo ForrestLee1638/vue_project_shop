96fb9bc2b3164d2c53656f71a6befd1053af9cd6
forrest <1638967263@qq.com>
2021-10-13 16:26:21 +0800

    后台管理系统

---
+++ src/components/Welcome.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3> The old man and the sea</h3>
+    <p>
+      He was an old man who fished alone in a skiff in the Gulf stream and he had gone eighty four days now without taking a fish. In the first forty days a boy had been with him. But after forty days without a fish the boy's parents had told him that the old man was now definitely and finally salao. which is the worst form of unluckly, and the boy had gone at their orders in another boat which caught three good fish the first week. It made the boy sad to see the old man come in each day with his skiff empty and he always went down to help him carry either the coiled lines or the gaff and harpoon and the sail that was furied around the mast. The looked like the flag of permanent dafeat.
+      The old man was thin and gaunt with deep wrinkils in the back of his neck. the brown blotches of the benevolent skin cancer the sun brings from its reflestion on the tropic sea were on his cheeks. The bolthes ran wll down the sides of his face and his hands had the deep-creased scars from handling heavy fish on the cords, But none of these scars were fresh. They were as old as erosions in a fishless desert.
+    </p>
+  </div>
+</template>
